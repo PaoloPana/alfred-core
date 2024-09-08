323ca416d39c0c4e113980e026576e3357cdcb65
Paolo Panarese <panarese.paolo@gmail.com>
2024-09-09 00:51:55 +0200

    WIP runner

--- src/bin/runner.rs
+++ src/bin/runner.rs
@@ -2,13 +2,14 @@ use std::env;
 use std::error::Error;
 use log::info;
 use alfred_rs::config::Config;
+use std::process::Command;
 
 #[cfg(feature = "runner")]
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
+    let config = Config::read(Some("runner".into()))?;
     let modules = if args.len() == 1 {
         println!("Args is empty, loading from config.toml...");
-        let config = Config::read(None)?;
         let alfred_config = Config::read_alfred_config()?;
         println!("{:?}", config);
         alfred_config.get_modules().unwrap()
@@ -17,5 +18,25 @@ fn main() -> Result<(), Box<dyn Error>> {
         args
     };
     println!("{:?}", modules);
+    let rust_log_env = if let Some(log) = config.get_module_value("log".into()) {
+        if matches!(log.as_str(), "debug" | "info" | "warn" | "error") {
+            log
+        } else {
+            "".into()
+        }
+    } else {
+        "".into()
+    };
+
+    modules.iter().for_each(|module| {
+        // TODO: check if file exists
+        info!("running module '{}'...", module);
+        Command::new("sh")
+            .arg("-c")
+            .env("RUST_LOG", rust_log_env.clone())
+            .arg(format!("./{module}"))
+            .spawn()
+            .expect("failed to execute process");
+    });
     Ok(())
 }
